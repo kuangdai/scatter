b7f34279b467682ba5a2f08723c809e6bd338e21
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2023-07-25 16:31:38 +0100

    1d

--- simulations/prem_5s__no_3d_mantle_crust__no_hetero/submit.sh
+++ simulations/prem_5s__no_3d_mantle_crust__no_hetero/submit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --time=02:00:00
+#SBATCH --time=01:00:00
 #SBATCH --partition=pi_korenaga
-#SBATCH --nodes=2
-#SBATCH --ntasks=96
+#SBATCH --nodes=1
+#SBATCH --ntasks=48
 #SBATCH --job-name=prem_5s__no_3d_mantle_crust__no_hetero
 
 cp ../../build/axisem3d ./
-mpirun -np 96 ./axisem3d
+mpirun -np 48 ./axisem3d
