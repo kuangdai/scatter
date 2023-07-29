d856dbeb18052f0028fc87e2847f3c4d5fcfcf27
Kuangdai Leng <kuangdai.leng@earth.ox.ac.uk>
2023-07-29 13:19:35 +0100

    reload modules before mpirun

--- simulations/prem_5s__no_3d_mantle_crust__no_hetero/submit.sh
+++ simulations/prem_5s__no_3d_mantle_crust__no_hetero/submit.sh
@@ -6,4 +6,8 @@
 #SBATCH --job-name=prem_5s__no_3d_mantle_crust__no_hetero
 
 cp ../../build/axisem3d ./
+module load foss/2018b
+module load METIS/5.1.0-GCCcore-7.3.0-32bit
+module load netCDF/4.6.1-foss-2018b
+
 mpirun -np 48 ./axisem3d
--- templates/submit.sh
+++ templates/submit.sh
@@ -3,4 +3,8 @@ __HEAD__
 #SBATCH --job-name=__NAME__
 
 cp ../../build/axisem3d ./
+module load foss/2018b
+module load METIS/5.1.0-GCCcore-7.3.0-32bit
+module load netCDF/4.6.1-foss-2018b
+
 mpirun -np __NP__ ./axisem3d
